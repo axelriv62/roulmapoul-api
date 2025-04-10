368e4565ab617481a7dde757d1151475fe307415
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 19:06:01 +0200

    fix: correction du fichier de migration rental_has_options
    closes #51

--- database/migrations/2025_04_10_134902_create_rental_has_options_table.php
+++ database/migrations/2025_04_10_134902_create_rental_has_options_table.php
@@ -1,17 +1,17 @@
 <?php
 
+use App\Models\Option;
+use App\Models\Rental;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('rental_has_options', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('option_id')->constrained('options')->onDelete('cascade');
-            $table->foreignId('rental_id')->constrained('rentals')->onDelete('cascade');
+            $table->foreignIdFor(Rental::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(Option::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }

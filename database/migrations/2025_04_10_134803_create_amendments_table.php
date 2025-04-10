60361bbae1b41d5af426aabf294f628f993ffefa
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 19:01:57 +0200

    fix: correction du fichier de migration amendments

--- database/migrations/2025_04_10_134803_create_amendments_table.php
+++ database/migrations/2025_04_10_134803_create_amendments_table.php
@@ -1,18 +1,19 @@
 <?php
 
+use App\Models\Rental;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('amendments', function (Blueprint $table) {
-            $table->id('id');
+            $table->id();
             $table->string('name');
+            $table->float('price');
             $table->string('content');
-            $table->foreignId('rental_id')->constrained('rentals')->onDelete('cascade');
+            $table->foreignIdFor(Rental::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }

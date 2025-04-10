844b4657daae211df1cd8177d9d1bde5e191c5cc
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 18:59:25 +0200

    fix: correction du fichier de migration documents

--- database/migrations/2025_04_10_134541_create_documents_table.php
+++ database/migrations/2025_04_10_134541_create_documents_table.php
@@ -5,15 +5,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('documents', function (Blueprint $table) {
-            $table->id('id');
-            $table->enum('type', ['type1', 'type2', 'type3']); //TODO Remplacer par les types réels
+            $table->id();
+            // TODO: type avec enum
             $table->string('url');
-            $table->foreignIdfor(Rental::class)->constrained('rentals')->onDelete('cascade');
+            $table->foreignIdfor(Rental::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }

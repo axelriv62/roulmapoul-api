69b16aafd5b2c8265dd4ceab5752bb8168e1ef88
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 15:57:46 +0200

    feat: ajout du modèle Withdrawal

---
+++ app/Models/Withdrawal.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Un retrait de véhicule.
+ *
+ * @property int $id
+ * @property Carbon $datetime
+ * @property int $user_id
+ * @property int $rental_id
+ */
+class Withdrawal extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'withdrawal';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'datetime',
+        'user_id',
+        'rental_id'
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'datetime' => 'datetime',
+    ];
+}

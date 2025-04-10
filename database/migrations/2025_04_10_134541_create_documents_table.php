6ec5678d20a74872b66035e698630a3fbec772e0
bylel_longelin <bylel_longelin@ens-univ.artois.fr>
2025-04-10 16:28:56 +0200

    Fix du migration files 2025_04_10_134541_create_documents_table.php

--- database/migrations/2025_04_10_134541_create_documents_table.php
+++ database/migrations/2025_04_10_134541_create_documents_table.php
@@ -9,10 +9,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('documents', function (Blueprint $table) {
-            $table->id('doc_id');
-            $table->enum('doc_type', ['type1', 'type2', 'type3']); // Remplacer par les types réels
-            $table->string('doc_url');
-            $table->foreignId('rental_id')->constrained('rentals')->onDelete('cascade');
+            $table->id('id');
+            $table->enum('type', ['type1', 'type2', 'type3']); // Remplacer par les types réels
+            $table->string('url');
+            $table->foreignId('rentals_id')->constrained('rentals')->onDelete('cascade');
+            //Si bug à cette table c'est surêment à cause de la ligne au dessus (RETIRER 'S' À RENTALS_ID
             $table->timestamps();
         });
     }

f9165d314d9499956ac72a0d6b4003ceb7c3faa9
bylel_longelin <bylel_longelin@ens-univ.artois.fr>
2025-04-10 16:27:57 +0200

    Fix du migration files 2025_04_10_134418_create_rentals_table.php

--- database/migrations/2025_04_10_134418_create_rentals_table.php
+++ database/migrations/2025_04_10_134418_create_rentals_table.php
@@ -9,13 +9,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('rentals', function (Blueprint $table) {
-            $table->id('rental_id');
-            $table->dateTime('rental_start');
-            $table->dateTime('rental_end');
-            $table->json('rental_options')->nullable();
-            $table->integer('rental_nb_days');
-            $table->float('rental_total_price');
-            $table->foreignId('cust_id')->constrained('customers')->onDelete('cascade');
+            $table->id('id');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->json('options')->nullable();
+            $table->integer('nb_days');
+            $table->float('total_price');
+            $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
             $table->string('car_registration');
             $table->foreign('car_registration')->references('car_registration')->on('cars')->onDelete('cascade');
             $table->timestamps();

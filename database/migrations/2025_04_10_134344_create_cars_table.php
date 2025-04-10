b16c81f9672d519b6b0a9973fc0b49ebd9c2018f
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 18:46:30 +0200

    fix: correction du fichier de migration cars

--- database/migrations/2025_04_10_134344_create_cars_table.php
+++ database/migrations/2025_04_10_134344_create_cars_table.php
@@ -1,21 +1,19 @@
 <?php
 
+use App\Models\Agency;
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->string('plate')->primary();
-            $table->enum('type', ['type1', 'type2', 'type3']);//TODO ajouter les bonnes énumérations
-            $table->string('condition');
-            $table->float('remaining_gas');
             $table->float('price_day');
-            $table->foreignId('agency_id')->constrained('agencies')->onDelete('cascade');
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignIdFor(Agency::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(Category::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }

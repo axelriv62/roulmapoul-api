ef07e616f18c87daede256f6693b7f07236a9151
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 18:54:01 +0200

    fix: correction du fichier de migration rentals

--- database/migrations/2025_04_10_134418_create_rentals_table.php
+++ database/migrations/2025_04_10_134418_create_rentals_table.php
@@ -1,21 +1,22 @@
 <?php
 
+use App\Models\Car;
+use App\Models\Customer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('rentals', function (Blueprint $table) {
-            $table->id('id');
+            $table->id();
             $table->dateTime('start');
             $table->dateTime('end');
             $table->integer('nb_days');
             $table->float('total_price');
-            $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
-            $table->foreign('car_registration')->references('car_registration')->on('cars')->onDelete('cascade');
+            $table->foreignIdFor(Customer::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(Car::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }

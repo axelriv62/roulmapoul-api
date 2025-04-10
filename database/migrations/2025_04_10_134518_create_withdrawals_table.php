c693d994c473de82466d04e3dd26e87d8c15b396
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 18:55:22 +0200

    fix: correction du fichier de migration withdrawals

--- database/migrations/2025_04_10_134518_create_withdrawals_table.php
+++ database/migrations/2025_04_10_134518_create_withdrawals_table.php
@@ -1,17 +1,24 @@
 <?php
 
+use App\Models\Agency;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('withdrawals', function (Blueprint $table) {
-            $table->id('id');
-            $table->datetime('hour');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->id();
+            $table->float('fuel_level');
+            $table->string('interior_condition');
+            $table->string('exterior_condition');
+            $table->float('mileage');
+            $table->datetime('datetime');
+            $table->string('comment')->nullable();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignIdFor(Agency::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }

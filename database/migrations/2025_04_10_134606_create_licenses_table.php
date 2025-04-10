28a06cbd168d6dafd5bc8cfef3904b29a0071f68
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 19:00:02 +0200

    fix: correction du fichier de migration licenses

--- database/migrations/2025_04_10_134606_create_licenses_table.php
+++ database/migrations/2025_04_10_134606_create_licenses_table.php
@@ -1,19 +1,19 @@
 <?php
 
+use App\Models\Customer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('licenses', function (Blueprint $table) {
             $table->string('num')->primary();
-            $table->date('date_acquisition');
-            $table->string('date_distribution');
+            $table->date('acquirement_date');
+            $table->date('distribution_date');
             $table->string('country');
-            $table->foreignId('customer_id')->nullable()->constrained('customers')->nullOnDelete();
+            $table->foreignIdFor(Customer::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }

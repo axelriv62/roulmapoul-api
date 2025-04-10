5844faa1d0c000179f28f0fa8edcd92e9ce5d179
bylel_longelin <bylel_longelin@ens-univ.artois.fr>
2025-04-10 16:28:35 +0200

    Fix du migration files 2025_04_10_134518_create_withdrawals_table.php

--- database/migrations/2025_04_10_134518_create_withdrawals_table.php
+++ database/migrations/2025_04_10_134518_create_withdrawals_table.php
@@ -9,8 +9,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('withdrawals', function (Blueprint $table) {
-            $table->id('withdrawal_id');
-            $table->date('withdraw_hour');
+            $table->id('id');
+            $table->date('hour');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });

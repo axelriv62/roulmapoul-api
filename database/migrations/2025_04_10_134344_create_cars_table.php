ddabe523bee3f2561ee2098e89689a4efa537289
bylel_longelin <bylel_longelin@ens-univ.artois.fr>
2025-04-10 16:27:35 +0200

    Fix du migration files 2025_04_09_134344_create_cars_table.php

--- database/migrations/2025_04_10_134344_create_cars_table.php
+++ database/migrations/2025_04_10_134344_create_cars_table.php
@@ -9,11 +9,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cars', function (Blueprint $table) {
-            $table->string('car_registration')->primary();
-            $table->enum('car_type', ['type1', 'type2', 'type3']);
-            $table->string('car_condition');
-            $table->float('car_remaining_gas');
-            $table->float('car_price_day');
+            $table->string('registration')->primary();
+            $table->enum('type', ['type1', 'type2', 'type3']);
+            $table->string('condition');
+            $table->float('remaining_gas');
+            $table->float('price_day');
             $table->foreignId('agency_id')->constrained('agencies')->onDelete('cascade');
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();

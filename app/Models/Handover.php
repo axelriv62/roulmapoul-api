9a541ee0c90a0ecb88edf5f434f3637caaa4ce7e
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 20:30:55 +0200

    fix: modification du modèle Handover

--- app/Models/Handover.php
+++ app/Models/Handover.php
@@ -6,10 +6,15 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Un retour.
+ * Un retour d'une voiture de location.
  *
  * @property int $id
+ * @property float $fuel_level
+ * @property string $interior_condition
+ * @property string $exterior_condition
+ * @property float $mileage
  * @property Carbon datetime
+ * @property string $comment
  * @property int $user_id
  * @property int $rental_id
  */
@@ -29,15 +34,22 @@ class Handover extends Model
      * @var string[]
      */
     protected $fillable = [
+        'fuel_level',
+        'interior_condition',
+        'exterior_condition',
+        'mileage',
         'datetime',
+        'comment',
         'user_id',
-        'rental_id'
+        'rental_id',
     ];
 
     /**
      * @var string[]
      */
     protected $casts = [
-        'datetime' => 'datetime',
+        'fuel_level' => 'float',
+        'mileage' => 'float',
+        'datetime' => 'datetime'
     ];
 }

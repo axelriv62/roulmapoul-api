5676c6c7378553bea9cfa6200e5bf27766298f15
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 19:38:52 +0200

    feat: ajout de méthodes pour transformer les enums en tableau

--- app/Enums/DocumentType.php
+++ app/Enums/DocumentType.php
@@ -7,4 +7,18 @@ enum DocumentType: string
     case BILL = 'bill';
     case WITHDRAWAL = 'withdrawal';
     case HANDOVER = 'handover';
+
+    public static function toValuesArray(): array
+    {
+        return array_map(fn($type) => $type->value, self::toArray());
+    }
+
+    public static function toArray(): array
+    {
+        return [
+            self::BILL,
+            self::WITHDRAWAL,
+            self::HANDOVER
+        ];
+    }
 }
--- app/Enums/RentalState.php
+++ app/Enums/RentalState.php
@@ -8,4 +8,19 @@ enum RentalState: string
     case CANCELLED = 'cancelled';
     case ONGOING = 'ongoing';
     case COMPLETED = 'completed';
+
+    public static function toValuesArray(): array
+    {
+        return array_map(fn(self $state) => $state->value, self::toArray());
+    }
+
+    public static function toArray(): array
+    {
+        return [
+            self::PAID,
+            self::CANCELLED,
+            self::ONGOING,
+            self::COMPLETED
+        ];
+    }
 }

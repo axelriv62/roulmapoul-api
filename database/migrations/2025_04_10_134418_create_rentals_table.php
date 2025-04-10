4edb3fc1aea9c1c11935479da2936d96402eb64c
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-10 19:33:22 +0200

    feat: ajout de l'état d'une location

--- database/migrations/2025_04_10_134418_create_rentals_table.php
+++ database/migrations/2025_04_10_134418_create_rentals_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\RentalState;
 use App\Models\Car;
 use App\Models\Customer;
 use Illuminate\Database\Migrations\Migration;
@@ -14,6 +15,7 @@ return new class extends Migration {
             $table->dateTime('start');
             $table->dateTime('end');
             $table->integer('nb_days');
+            $table->enum('state', RentalState::cases());
             $table->float('total_price');
             $table->foreignIdFor(Customer::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignIdFor(Car::class, 'car_plate')->constrained()->cascadeOnDelete()->cascadeOnUpdate();

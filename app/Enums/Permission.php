df971505b369c9e2a2d8016ce3fa75e5dc51517c
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-11 17:09:17 +0200

    fix: modification d'une permission qui était mal définie

--- app/Enums/Permission.php
+++ app/Enums/Permission.php
@@ -6,7 +6,7 @@ enum Permission: string
 {
     case CREATE_RENTAL = 'rental.create';
     case UPDATE_RENTAL = 'rental.update';
-    case READ_RENTAL = 'rental.read';
+    case READ_ALL_RENTAL = 'rental.all.read';
     case CREATE_WITHDRAWAL = 'withdrawal.create';
     case CREATE_HANDOVER = 'handover.create';
     case CREATE_AMENDMENT = 'amendment.create';

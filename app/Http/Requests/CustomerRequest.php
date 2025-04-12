bc9e2d2ea6a20937328782ba7c3c666d4fcc0f4b
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-12 12:49:43 +0200

    feature: création de la classe request pour les clients

---
+++ app/Http/Requests/CustomerRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "first_name" => "required|string|between:4,50",
+            "last_name" => "required|string|between:4,50",
+            "num" => "required|string|between:1,5",
+            "street" => "required|string|between:4,50",
+            "zip" => "required|string|size:5",
+            "city" => "required|string|between:2,50",
+            "country" => "required|string|between:2,50",
+            "phone" => "required|string|between:4,50",
+            "email" => "required|string|email|max:255|unique:users,email," . $this->user()->id,
+        ];
+    }
+
+    /**
+     * Customise les messages de validation.
+     *
+     * @return array<string, string> les messages de validation.
+     */
+    public function messages(): array
+    {
+        return [
+            "required" => "Le champ :attribute est requis.",
+            "string" => "Le champ :attribute doit être une chaîne de caractères.",
+            "between" => "Le champ :attribute doit contenir entre :min et :max caractères.",
+            "size" => "Le champ :attribute doit contenir :size caractères.",
+            "email" => "Le champ :attribute doit être une adresse e-mail valide.",
+            "unique" => "Cette adresse e-mail est déjà utilisée.",
+            "max" => "Le champ :attribute ne doit pas dépasser :max caractères.",
+        ];
+    }
+}

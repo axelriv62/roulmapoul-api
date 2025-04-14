76625e03f74926ad7121e3e61f09b1b32f5f1428
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-14 12:39:13 +0200

    feat: ajout de la factory des retraits

---
+++ database/factories/WithdrawalFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Rental;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Withdrawal>
+ */
+class WithdrawalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'fuel_level'         => $this->faker->randomFloat(2, 0.01, 50),
+            'interior_condition' => $this->faker->text(20),
+            'exterior_condition' => $this->faker->text(20),
+            'mileage'            => $this->faker->randomFloat(2, 0.01, 200000),
+            'datetime'           => $this->faker->dateTime(),
+            'comment'            => $this->faker->text(),
+            'user_id'            => User::factory(),
+            'rental_id'          => Rental::factory()
+        ];
+    }
+}

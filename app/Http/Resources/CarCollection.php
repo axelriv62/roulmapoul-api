8e0a115fe45c6fe891796895ed8a936cdb3450bf
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-18 17:20:38 +0200

    feat: écriture de toutes classes collections (même logique pour toutes)

--- app/Http/Resources/CarCollection.php
+++ app/Http/Resources/CarCollection.php
@@ -14,6 +14,11 @@ class CarCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'links' => [
+                'self' => 'link-value', // TODO Remplacer par le bon lien (route('cars.index'))
+            ],
+        ];
     }
 }
--- app/Http/Resources/CategoryCollection.php
+++ app/Http/Resources/CategoryCollection.php
@@ -14,6 +14,11 @@ class CategoryCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'links' => [
+                'self' => 'link-value', // TODO Remplacer par le bon lien (route('categories.index'))
+            ],
+        ];
     }
 }

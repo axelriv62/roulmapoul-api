aba69efa6404ff5164764694bfb4f1ec0a1f6a8f
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-18 16:00:03 +0200

    feat: création de la request pour les retours

--- app/Http/Requests/HandoverRequest.php
+++ app/Http/Requests/HandoverRequest.php
@@ -12,7 +12,7 @@ class HandoverRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true; // TODO changer pour que seuls les agents puissent le faire
     }
 
     /**
@@ -23,7 +23,29 @@ class HandoverRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "datetime" => "required|date_format:Y-m-d H:i:s",
+            "mileage" => "required|integer|min:0",
+            "fuel_level" => "required|numeric|min:0",
+            "interior_condition" => "string|between:0,500",
+            "exterior_condition" => "string|between:0,500",
+            "comment" => "string|between:0,500",
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            "required" => "Le champ :attribute est requis.",
+            "string" => "Le champ :attribute doit être une chaîne de caractères.",
+            "integer" => "Le champ :attribute doit être un entier.",
+            "numeric" => "Le champ :attribute doit être un nombre.",
+            "between" => "Le champ :attribute doit contenir entre :min et :max caractères.",
+            "date_format" => "Le champ :attribute doit être au format :format."
         ];
     }
 }

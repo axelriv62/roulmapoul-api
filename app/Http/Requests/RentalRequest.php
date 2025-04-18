534dad713fd7f02251dbdc1adfe97ac1787fdabf
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-18 16:09:57 +0200

    feat: request pour les locations

--- app/Http/Requests/RentalRequest.php
+++ app/Http/Requests/RentalRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RentalRequest extends FormRequest
@@ -11,18 +12,40 @@ class RentalRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true; // TODO : Implémenter la logique d'autorisation
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'customer_id' => 'required|exists:customers,id',
+            'car_plate' => 'required|exists:cars,plate',
+            'warranty_id' => 'nullable|exists:warranties,id',
+            'options' => 'nullable|array',
+            'options.*' => 'integer|exists:options,id',
+            'start' => 'required|date_format:Y-m-d|after_or_equal:today',
+            'end' => 'required|date_format:Y-m-d|after:start',
+        ];
+    }
+
+    /**
+     * Get the custom messages for the validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'Le champ :attribute est requis.',
+            'exists' => 'Le champ :attribute doit exister dans la base de données.',
+            'date_format' => 'Le champ :attribute doit être au format :format.',
+            'after_or_equal' => 'La date de début de la location doit être aujourd\'hui ou une date ultérieure.',
+            'after' => 'La fin de la location doit se faire après le début.',
         ];
     }
 }

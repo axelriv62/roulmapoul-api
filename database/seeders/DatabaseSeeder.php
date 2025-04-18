b6e7ecbf7e4a0c8dad725b34264570b63d124de4
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-19 00:27:35 +0200

    feat: ajout du rôle suprême au grand robert et création de 4 agents

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -25,14 +26,48 @@ class DatabaseSeeder extends Seeder
             RentalSeeder::class,
             HandoverSeeder::class,
             LicenseSeeder::class,
-            WithdrawalSeeder::class
+            WithdrawalSeeder::class,
+            RoleSeeder::class
         ]);
 
-        User::factory()->create([
+        $robert = User::factory()->create([
             'name' => 'robert.duchmol',
             'email' => 'robert.duchmol@roulmapoul.fr',
             'password' => Hash::make('GrosSecret'),
             'email_verified_at' => now(),
         ]);
+        $robert->assignRole(Role::ADMIN->value);
+
+        $carl = User::factory()->create([
+            'name' => 'carl.kolodziejski',
+            'email' => 'carl.kolodziejski@roulmapoul.fr',
+            'password' => Hash::make('GrosSecret'),
+            'email_verified_at' => now(),
+        ]);
+        $carl->assignRole(Role::AGENT->value);
+
+        $axel = User::factory()->create([
+            'name' => 'axel.riviere',
+            'email' => 'axel.riviere@roulmapoul.fr',
+            'password' => Hash::make('GrosSecret'),
+            'email_verified_at' => now(),
+        ]);
+        $axel->assignRole(Role::AGENT->value);
+
+        $quentin = User::factory()->create([
+            'name' => 'quentin.tripognez',
+            'email' => 'quentin.tripognez@roulmapoul.fr',
+            'password' => Hash::make('GrosSecret'),
+            'email_verified_at' => now(),
+        ]);
+        $quentin->assignRole(Role::AGENT->value);
+
+        $bylel = User::factory()->create([
+            'name' => 'bylel.longelin',
+            'email' => 'bylel.longelin@roulmapoul.fr',
+            'password' => Hash::make('GrosSecret'),
+            'email_verified_at' => now(),
+        ]);
+        $bylel->assignRole(Role::AGENT->value);
     }
 }

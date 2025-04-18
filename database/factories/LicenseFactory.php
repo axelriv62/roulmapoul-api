f39f2a6392bc5b9da8fbc6e0b62b3d044f209c1b
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-18 21:48:04 +0200

    fix: modification de la factory des permis pour avoir des dates d'acquisitions et de distributions cohérentes

--- database/factories/LicenseFactory.php
+++ database/factories/LicenseFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\License;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,11 +18,14 @@ class LicenseFactory extends Factory
      */
     public function definition(): array
     {
+        $acquirement_date = Carbon::createFromFormat('Y-m-d', $this->faker->dateTimeBetween('-80 years', '-18 years')->format('Y-m-d'));
+        $distribution_date = $acquirement_date->copy()->addDays(rand(1, 10));
+
         return [
             'num' => $this->faker->unique()->numerify('############'),
             'birthday' => $this->faker->dateTimeBetween('-80 years', '-18 years')->format('Y-m-d'),
-            'acquirement_date' => $this->faker->date(),
-            'distribution_date' => $this->faker->date(),
+            'acquirement_date' => $acquirement_date,
+            'distribution_date' => $distribution_date,
             'country' => $this->faker->country(),
         ];
     }

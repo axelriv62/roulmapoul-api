93623bfd48d5b027e4a009c4b562d933372c8eeb
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-18 21:26:10 +0200

    fix: correction de la détermination des états de locations dans leur factory

--- database/factories/RentalFactory.php
+++ database/factories/RentalFactory.php
@@ -22,9 +22,16 @@ class RentalFactory extends Factory
         $start = Carbon::now()->addDays(rand(-60, 60));
         $end = $start->copy()->addDays(rand(7, 30));
 
-        $state = Carbon::now()->between($start, $end)
-            ? RentalState::ONGOING->value
-            : $this->faker->randomElement(RentalState::toValuesArray());
+        $state = RentalState::CANCELLED->value;
+        if (Carbon::now()->isBefore($start)) {
+            if (rand(0, 100) > 20) {
+                $state = RentalState::PAID->value;
+            }
+        } elseif (Carbon::now()->between($start, $end)) {
+            $state = RentalState::ONGOING->value;
+        } elseif (Carbon::now()->isAfter($end)) {
+            $state = RentalState::COMPLETED->value;
+        }
 
         return [
             'start' => $start->format('Y-m-d'),

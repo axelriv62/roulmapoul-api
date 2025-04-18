b53d032f5b69cce48870ce76b9c5cad31949816a
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-18 17:27:12 +0200

    feat: classes resource pour les informations de facturation

--- app/Http/Resources/BillingAddressResource.php
+++ app/Http/Resources/BillingAddressResource.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin Customer
+ */
 class BillingAddressResource extends JsonResource
 {
     /**
@@ -14,6 +18,12 @@ class BillingAddressResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "num_bill" => $this->num_bill,
+            "street_bill" => $this->street_bill,
+            "zip_bill" => $this->zip_bill,
+            "city_bill" => $this->city_bill,
+            "country_bill" => $this->country_bill
+        ];
     }
 }

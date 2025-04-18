40e8ef36eced58f51981f79ac40d38cdfb8dc8c4
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-18 16:01:14 +0200

    feat: création de la request pour les demandes de connexion

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
@@ -11,18 +12,34 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            "email" => "required|email",
+            "password" => "required|string|min:6",
+        ];
+    }
+
+    /**
+     * Get the custom messages for the validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            "required" => "Le champ :attribute est requis.",
+            "email" => "Le champ :attribute doit être une adresse e-mail valide.",
+            "string" => "Le champ :attribute doit être une chaîne de caractères.",
+            "min" => "Le champ :attribute doit contenir au moins :min caractères.",
         ];
     }
 }

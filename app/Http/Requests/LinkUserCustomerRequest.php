50f5aa94ee2e4f09a96ebf4c7c623704e709539d
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-19 20:18:10 +0200

    feat: liaison d'un client avec un utilisateur

---
+++ app/Http/Requests/LinkUserCustomerRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LinkUserCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "required|string|max:255|unique:users,name",
+            "password" => "required|string|min:8"
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            "required" => "Le champ :attribute est requis.",
+            "string" => "Le champ :attribute doit être une chaîne de caractères.",
+            "max" => "Le champ :attribute ne doit pas dépasser :max caractères.",
+            "name.unique" => "Ce nom d'utilisateur est déjà pris.",
+            "min" => "Le champ :attribute doit contenir au moins :min caractères.",
+        ];
+    }
+}

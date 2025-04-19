9fa9f3dcd16a5eec99e42bd09846c1fb04228466
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-19 15:02:14 +0200

    fix: infos du client obligatoires

--- database/migrations/2025_04_09_205252_create_customers_table.php
+++ database/migrations/2025_04_09_205252_create_customers_table.php
@@ -10,22 +10,21 @@ return new class extends Migration {
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email');
-            $table->string('phone');
-            $table->string('num');
-            $table->string('street');
-            $table->string('zip');
-            $table->string('city');
-            $table->string('country');
-            $table->string('num_bill');
-            $table->string('street_bill');
-            $table->string('zip_bill');
-            $table->string('city_bill');
-            $table->string('country_bill');
+            $table->string('first_name')->nullable(false);
+            $table->string('last_name')->nullable(false);
+            $table->string('email')->nullable(false);
+            $table->string('phone')->nullable(false);
+            $table->string('num')->nullable(false);
+            $table->string('street')->nullable(false);
+            $table->string('zip')->nullable(false);
+            $table->string('city')->nullable(false);
+            $table->string('country')->nullable(false);
+            $table->string('num_bill')->nullable(false);
+            $table->string('street_bill')->nullable(false);
+            $table->string('zip_bill')->nullable(false);
+            $table->string('city_bill')->nullable(false);
+            $table->string('country_bill')->nullable(false);
             $table->foreignIdFor(User::class)->nullable()->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-
             $table->timestamps();
         });
     }

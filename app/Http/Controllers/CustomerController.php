632d6cfb97226a401e265172614aa839abacd5a1
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-19 18:28:31 +0200

    feat: filtrage de la liste des clients

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -12,14 +12,25 @@ class CustomerController extends BaseController
 {
     /**
      * Liste les clients.
+     * Seuls les agents et les administrateurs peuvent accéder à cette méthode.
+     *
+     * @param Request $request La requête HTTP qui contient les paramètres de filtrage.
      */
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
         if (Auth::user()->cannot('readAny', Customer::class)) {
             return $this->sendError('Non autorisé.', 'Vous n\'êtes pas autorisé à effectuer cette opération.', 403);
         }
-        
-        $customers = Customer::all();
+
+        $filters = $request->query('filter', []);
+
+        $customers = Customer::query()
+            ->when(isset($filters['first_name']), fn($query) => $query->where('first_name', 'like', '%' . $filters['first_name'] . '%'))
+            ->when(isset($filters['email']), fn($query) => $query->where('email', 'like', '%' . $filters['email'] . '%'))
+            ->when(isset($filters['phone']), fn($query) => $query->where('phone', 'like', '%' . $filters['phone'] . '%'))
+            ->when(isset($filters['rental_id']), fn($query) => $query->where('rental_id', $filters['rental_id']))
+            ->get();
+
         $success = new CustomerCollection($customers);
         return $this->sendResponse($success, "Liste des clients retrouvées avec succès.");
     }

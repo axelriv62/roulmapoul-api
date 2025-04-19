7a88e78fe680b443050eed096377a58dd9f0a7ab
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-19 13:06:14 +0200

    fix: modification de AgentRegisterRequest pour authoriser seulement les administrateurs

--- app/Http/Requests/AgentRegisterRequest.php
+++ app/Http/Requests/AgentRegisterRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\Role;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AgentRegisterRequest extends FormRequest // Permet aux administrateurs de créer des agents.
 {
@@ -12,7 +14,7 @@ class AgentRegisterRequest extends FormRequest // Permet aux administrateurs de 
      */
     public function authorize(): bool
     {
-        return false; // TODO changer pour Auth::user()->hasRole(Role::ADMIN) quand le système de rôle sera mis en place.
+        return Auth::user()->hasRole(Role::ADMIN);
     }
 
     /**

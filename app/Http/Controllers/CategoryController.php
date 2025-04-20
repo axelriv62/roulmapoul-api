c700ca7a81101911611ebf6c97e9848a602e3de5
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-20 18:50:27 +0200

    feat: filtrage de la liste des catégories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,12 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class CategoryController extends BaseController
 {
-    public function index(): JsonResponse
+    /**
+     * Liste les catégories.
+     *
+     * @param Request $request La requête HTTP qui contient les paramètres de filtrage.
+     */
+    public function index(Request $request): JsonResponse
     {
-        $success = new CategoryCollection(Category::all());
+        $filters = $request->query('filter', []);
+
+        $categories = Category::query()
+            ->when(isset($filters['name']), fn($query) => $query->where('name', 'like', '%' . $filters['name'] . '%'))
+            ->when(isset($filters['description']), fn($query) => $query->where('description', 'like', '%' . $filters['description'] . '%'))
+            ->get();
+
+        $success = new CategoryCollection($categories);
         return $this->sendResponse($success, "Liste des catégories retrouvées avec succès.");
     }
 }

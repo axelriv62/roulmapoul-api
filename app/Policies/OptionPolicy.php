ea0580ce051210216970460065868d941ce4f04e
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-20 20:02:30 +0200

    feat: policy des options

---
+++ app/Policies/OptionPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\Permission;
+use App\Models\User;
+
+class OptionPolicy
+{
+    /**
+     * Vérifie si l'utilisateur peut créer une option.
+     *
+     * @param User $user L'utilisateur qui effectue la demande.
+     * @return bool true si l'utilisateur peut créer une option, sinon false.
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasPermission(Permission::CREATE_OPTION);
+    }
+
+    /**
+     * Vérifie si l'utilisateur peut modifier une option.
+     *
+     * @param User $user L'utilisateur qui effectue la demande.
+     * @return bool true si l'utilisateur peut modifier une option, sinon false.
+     */
+    public function update(User $user): bool
+    {
+        return $user->hasPermission(Permission::UPDATE_OPTION);
+    }
+}

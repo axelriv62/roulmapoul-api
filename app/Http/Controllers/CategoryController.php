62de8ab4574757665c1571b6e7ccb1579b83aca1
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-20 19:01:11 +0200

    feat: modification d'une catégorie

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,6 +15,7 @@ class CategoryController extends BaseController
      * Liste les catégories.
      *
      * @param Request $request La requête HTTP qui contient les paramètres de filtrage.
+     * @return JsonResponse La réponse JSON contenant la liste des catégories.
      */
     public function index(Request $request): JsonResponse
     {
@@ -31,6 +32,9 @@ class CategoryController extends BaseController
 
     /**
      * Crée une nouvelle catégorie.
+     *
+     * @param CategoryRequest $request La requête HTTP contenant les données de la catégorie à créer.
+     * @return JsonResponse La réponse JSON contenant les détails de la catégorie créée.
      */
     public function store(CategoryRequest $request): JsonResponse
     {
@@ -41,5 +45,29 @@ class CategoryController extends BaseController
         $category = Category::create($request->validated());
         $success = new CategoryResource($category);
         return $this->sendResponse($success, "La catégorie a été créée avec succès.");
+    }
+
+    /**
+     * Modifie une catégorie existante.
+     *
+     * @param CategoryRequest $request La requête HTTP contenant les données de la catégorie à modifier.
+     * @param string $id L'identifiant de la catégorie à modifier.
+     * @return JsonResponse La réponse JSON contenant les détails de la catégorie modifiée.
+     */
+    public function update(CategoryRequest $request, string $id): JsonResponse
+    {
+        if (auth()->user()->cannot('update', Category::class)) {
+            return $this->sendError('Non autorisé.', 'Vous n\'êtes pas autorisé à effectuer cette opération.', 403);
+        }
+
+        $category = Category::find($id);
+
+        if (!$category) {
+            return $this->sendError('Non trouvé.', 'La catégorie demandée n\'existe pas.', 404);
+        }
+
+        $category->update($request->validated());
+        $success = new CategoryResource($category);
+        return $this->sendResponse($success, "La catégorie a été mise à jour avec succès.");
     }
 }

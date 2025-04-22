149761ed0d422a2341023e430e5238da004c7cd4
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-23 00:01:09 +0200

    test: contrôleur des voitures

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\CarCollection;
 use App\Http\Resources\CarResource;
 use App\Models\Car;
 use Illuminate\Http\JsonResponse;
---
+++ tests/Feature/CarControllerTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\CarAvailability;
+use App\Models\Agency;
+use App\Models\Car;
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CarControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Vérifie que la route de la liste des voitures est accessible.
+     */
+    public function test_index(): void
+    {
+        $response = $this->get('/api/cars');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Vérifie que les filtres de la liste des voitures fonctionnent.
+     */
+    public function test_index_filters(): void
+    {
+        $agency = Agency::factory()->create();
+        $category1 = Category::factory()->create();
+        $category2 = Category::factory()->create();
+        Car::factory()->create([
+            'agency_id' => $agency->id,
+            'category_id' => $category1->id,
+            'availability' => CarAvailability::AVAILABLE,
+        ]);
+        Car::factory()->create([
+            'agency_id' => $agency->id,
+            'category_id' => $category2->id,
+            'availability' => CarAvailability::AVAILABLE,
+        ]);
+
+        $response = $this->get('/api/cars?availability=available&category='.$category1->id);
+        $response->assertJsonCount(1, 'data.cars');
+    }
+
+    /**
+     * Vérifie que la route de la liste des voitures filtrées par agence est accessible.
+     */
+    public function test_index_agency(): void
+    {
+        $response = $this->get('/api/cars/agency/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Vérifie que le filtrage de la route de la liste des voitures filtrées par agence fonctionne.
+     */
+    public function test_index_agency_filter(): void
+    {
+        $agency1 = Agency::factory()->create();
+        $agency2 = Agency::factory()->create();
+        $category1 = Category::factory()->create();
+        $category2 = Category::factory()->create();
+        Car::factory()->create([
+            'agency_id' => $agency1->id,
+            'category_id' => $category1->id,
+            'availability' => CarAvailability::AVAILABLE,
+        ]);
+        Car::factory()->create([
+            'agency_id' => $agency1->id,
+            'category_id' => $category2->id,
+            'availability' => CarAvailability::AVAILABLE,
+        ]);
+        Car::factory()->create([
+            'agency_id' => $agency2->id,
+            'category_id' => $category1->id,
+            'availability' => CarAvailability::AVAILABLE,
+        ]);
+        Car::factory()->create([
+            'agency_id' => $agency2->id,
+            'category_id' => $category2->id,
+            'availability' => CarAvailability::AVAILABLE,
+        ]);
+
+        $response = $this->get('/api/cars/agency/'.$agency1->id.'?availability=available&category='.$category1->id);
+        $response->assertJsonCount(1, 'data.cars');
+    }
+}

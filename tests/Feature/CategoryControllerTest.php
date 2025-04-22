63db783e7af4ff5fd869fea5a63a1c61d258a893
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-23 00:05:18 +0200

    test: contrôleur des catégories

---
+++ tests/Feature/CategoryControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CategoryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Vérifie que la route de la liste des catégories fonctionne.
+     */
+    public function test_index():void
+    {
+        $response = $this->get(route('categories.index'));
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Vérifie que les catégories sont bien récupérées.
+     */
+    public function test_get_index():void
+    {
+        Category::factory(2)->create();
+        $response = $this->get(route('categories.index'));
+        $response->assertJsonCount(2, 'data.categories');
+    }
+}

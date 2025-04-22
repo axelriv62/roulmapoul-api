7b1559a451e5865cf6df7b5aec70d545087268c7
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-23 00:12:06 +0200

    test: contrôleur des garanties

---
+++ tests/Feature/WarrantyControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Warranty;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class WarrantyControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Vérifie que la route de la liste des catégories fonctionne.
+     */
+    public function test_index(): void
+    {
+        $response = $this->get(route('warranties.index'));
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Vérifie que les catégories sont bien récupérées.
+     */
+    public function test_get_index(): void
+    {
+        Warranty::factory(2)->create();
+        $response = $this->get(route('warranties.index'));
+        $response->assertJsonCount(2, 'data.warranties');
+    }
+}

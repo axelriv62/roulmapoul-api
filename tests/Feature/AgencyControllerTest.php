ea51a6c64799230fa6cef7f0a1b4618b8eb95aaf
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-22 23:39:13 +0200

    test: contrôleur des agences

---
+++ tests/Feature/AgencyControllerTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Agency;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AgencyControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Vérifie que la liste des agences est accessible.
+     */
+    public function test_index(): void
+    {
+        $response = $this->get('/api/agencies');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Vérifie que le filtrage de la liste des agences fonctionne.
+     */
+    public function test_filter_index(): void
+    {
+        Agency::factory()->create(
+            [
+                'city' => 'Paris',
+                'zip' => '75000',
+            ]
+        );
+        Agency::factory()->create(
+            [
+                'city' => 'Paris',
+                'zip' => '75195',
+            ]
+        );
+
+        $response = $this->get('/api/agencies?city=Paris&zip=75000');
+
+        $response->assertJsonCount(1, 'data.agencies');
+    }
+
+    /**
+     * Vérifie que le tri ascendant de la liste des agences fonctionne.
+     */
+    public function test_sort_index_asc(): void
+    {
+        Agency::factory()->create(
+            [
+                'name' => 'Agence',
+            ]
+        );
+        Agency::factory()->create(
+            [
+                'name' => 'Zoo',
+            ]
+        );
+
+        $response = $this->get('/api/agencies?sort=asc');
+
+        $response->assertJsonPath('data.agencies.0.name', 'Agence');
+    }
+
+    /**
+     * Vérifie que le tri descendant de la liste des agences fonctionne.
+     */
+    public function test_sort_index_desc(): void
+    {
+        Agency::factory()->create(
+            [
+                'name' => 'Agence',
+            ]
+        );
+        Agency::factory()->create(
+            [
+                'name' => 'Zoo',
+            ]
+        );
+
+        $response = $this->get('/api/agencies?sort=desc');
+
+        $response->assertJsonPath('data.agencies.0.name', 'Zoo');
+    }
+}

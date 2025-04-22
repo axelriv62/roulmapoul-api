40eff1c4551f6cbad0286f0f39c91f324d1847b0
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-23 00:09:38 +0200

    refactor: linting

--- tests/Feature/CategoryControllerTest.php
+++ tests/Feature/CategoryControllerTest.php
@@ -13,7 +13,7 @@ class CategoryControllerTest extends TestCase
     /**
      * Vérifie que la route de la liste des catégories fonctionne.
      */
-    public function test_index():void
+    public function test_index(): void
     {
         $response = $this->get(route('categories.index'));
         $response->assertStatus(200);
@@ -22,7 +22,7 @@ class CategoryControllerTest extends TestCase
     /**
      * Vérifie que les catégories sont bien récupérées.
      */
-    public function test_get_index():void
+    public function test_get_index(): void
     {
         Category::factory(2)->create();
         $response = $this->get(route('categories.index'));

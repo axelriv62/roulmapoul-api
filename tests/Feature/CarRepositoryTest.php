8cfa6715006fe2ce19770b0af5e1ea208864f641
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-22 17:17:37 +0200

    test: test et correction de la fonction CarRepository.php

---
+++ tests/Feature/CarRepositoryTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Feature;
+
+use App\Models\Agency;
+use App\Models\Car;
+use App\Models\Category;
+use App\Models\Customer;
+use App\Models\Rental;
+use App\Repositories\CarRepository;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class CarRepositoryTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Teste si une voiture est louable à une date donnée.
+     */
+    public function testCarIsRentable(): void
+    {
+        $agency = Agency::factory()->create();
+        $category = Category::factory()->create();
+        $customer = Customer::factory()->create();
+        $car = Car::factory()->create([
+            'agency_id' => $agency->id,
+            'category_id' => $category->id,
+        ]);
+
+        Rental::factory()->create([
+            'car_plate' => $car->plate,
+            'start' => now()->addDay(),
+            'end' => now()->addDays(5),
+            'customer_id' => $customer->id,
+        ]);
+
+        $this->assertTrue(CarRepository::isRentable($car->plate, now()->addDays(6), now()->addDays(10)));
+        $this->assertFalse(CarRepository::isRentable($car->plate, now()->addDays(3), now()->addDays(4)));
+        $this->assertFalse(CarRepository::isRentable($car->plate, now()->addDay(), now()->addDays(3)));
+    }
+}

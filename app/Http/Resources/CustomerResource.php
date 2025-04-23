7eb0a2c2ad25ce68661a8c55b14c705dfedb976d
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-23 15:16:58 +0200

    fix: correction de l'obtention du permis du client

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Models\Customer;
+use App\Models\License;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -27,7 +28,7 @@ class CustomerResource extends JsonResource
             'address' => $this->num.' '.$this->street.', '.$this->zip.' '.$this->city.', '.$this->country,
             'billing_address' => $this->num_bill.' '.$this->street_bill.', '.$this->zip_bill.' '.$this->city_bill.', '.$this->country_bill,
             'user_id' => $this->user_id ?: null,
-            'license' => $this->license,
+            'license' => array_diff_key((new LicenseResource($this->license))->toArray($request), array_flip(['customer_id'])),
         ];
     }
 }

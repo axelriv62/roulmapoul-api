0250fd00d4fc427982726f3535722ec501656edb
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-04-23 10:45:31 +0200

    feat: appel de la méthode isRentable dans la fonction index des agences

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CarCollection;
 use App\Http\Resources\CarResource;
 use App\Models\Car;
+use App\Repositories\CarRepository;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -13,7 +15,7 @@ class CarController extends BaseController
     /**
      * Liste les voitures.
      *
-     * @param  Request  $request  La requête HTTP qui contient les paramètres de filtrage.
+     * @param Request $request La requête HTTP qui contient les paramètres de filtrage.
      * @return JsonResponse La réponse JSON contenant la liste des voitures.
      */
     public function index(Request $request): JsonResponse
@@ -22,8 +24,8 @@ class CarController extends BaseController
         $availability = $request->query('availability');
 
         $cars = Car::query()
-            ->when($category, fn ($query) => $query->where('category_id', $category))
-            ->when($availability, fn ($query) => $query->where('availability', $availability))
+            ->when($category, fn($query) => $query->where('category_id', $category))
+            ->when($availability, fn($query) => $query->where('availability', $availability))
             ->get();
 
         $success['cars'] = CarResource::collection($cars);
@@ -34,20 +36,31 @@ class CarController extends BaseController
     /**
      * Liste les voitures d'une agence spécifique.
      *
-     * @param  Request  $request  La requête HTTP qui contient les paramètres de filtrage.
-     * @param  string  $id  L'identifiant de l'agence.
+     * @param Request $request La requête HTTP qui contient les paramètres de filtrage.
+     * @param string $id L'identifiant de l'agence.
      * @return JsonResponse La réponse JSON contenant la liste des voitures de l'agence.
      */
     public function indexAgency(Request $request, string $id): JsonResponse
     {
         $category = $request->query('category');
         $availability = $request->query('availability');
+        $start = $request->query('start');
+        $end = $request->query('end');
 
         $cars = Car::query()
             ->where('agency_id', $id)
-            ->when($category, fn ($query) => $query->where('category_id', $category))
-            ->when($availability, fn ($query) => $query->where('availability', $availability))
+            ->when($category, fn($query) => $query->where('category_id', $category))
+            ->when($availability, fn($query) => $query->where('availability', $availability))
             ->get();
+
+        if ($start && $end) {
+            $start = Carbon::createFromFormat('d-m-Y', $start)->startOfDay();
+            $end = Carbon::createFromFormat('d-m-Y', $end)->endOfDay();
+
+            $cars = $cars->filter(function ($car) use ($start, $end) {
+                return CarRepository::isRentable($car->plate, $start, $end);
+            });
+        }
 
         $success['cars'] = CarResource::collection($cars);
 

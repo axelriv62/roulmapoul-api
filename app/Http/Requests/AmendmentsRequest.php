ee8087624abf2678cde8a617fd419b5aade3b54e
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-24 02:07:44 +0200

    feat: ajout d'une classe request pour les avenants

---
+++ app/Http/Requests/AmendmentsRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AmendmentsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::user()->can('create', \App\Models\Amendment::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "amendments" => "nullable|array",
+            "amendments.*.name" => "required|string|between:4,50",
+            "amendments.*.price" => "required|numeric|min:0",
+            "amendments.*.description" => "nullable|string|between:4,50",
+            "amendments.*.rental_id" => "required|exists:rentals,id",
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'Le champ :attribute est requis.',
+            'string' => 'Le champ :attribute doit être une chaîne de caractères.',
+            'between' => 'Le champ :attribute doit contenir entre :min et :max caractères.',
+            'numeric' => 'Le champ :attribute doit être un nombre.',
+            'min' => 'Le champ :attribute doit être supérieur ou égal à :min.',
+            'exists' => 'La valeur sélectionnée pour le champ :attribute n\'existe pas.',
+        ];
+    }
+}

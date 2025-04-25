6e3056e6102ccd1ae3ab506f6dde5b4448002431
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-04-25 12:42:46 +0200

    fix: déplacement de la création des documents

--- app/Http/Controllers/WithdrawalController.php
+++ app/Http/Controllers/WithdrawalController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Enums\CarAvailability;
+use App\Enums\DocumentType;
 use App\Enums\RentalState;
 use App\Http\Requests\WithdrawalRequest;
 use App\Http\Resources\WithdrawalResource;
 use App\Jobs\MailWithdrawalJob;
 use App\Models\Rental;
 use App\Models\Withdrawal;
+use Dompdf\Dompdf;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class WithdrawalController extends BaseController
 {
@@ -43,6 +46,19 @@ class WithdrawalController extends BaseController
 
         $rental->car->availability = CarAvailability::RENTED;
         $rental->car->save();
+
+        $dompdf = new Dompdf;
+        $dompdf->loadHtml(view('pdf.withdrawal', ['withdrawal' => $withdrawal]));
+        $dompdf->setPaper('A4');
+        $dompdf->render();
+
+        $filePath = 'docs/withdrawal_'.$rental->customer->id.'_'.$withdrawal->id.'.pdf';
+        Storage::put($filePath, $dompdf->output());
+
+        $rental->documents()->create([
+            'type' => DocumentType::WITHDRAWAL,
+            'url' => $filePath,
+        ]);
 
         MailWithdrawalJob::dispatch($withdrawal, $rental->customer);
         $success['withdrawal'] = new WithdrawalResource($withdrawal);
--- app/Jobs/MailBillJob.php
+++ app/Jobs/MailBillJob.php
@@ -5,9 +5,7 @@ namespace App\Jobs;
 use App\Enums\DocumentType;
 use App\Mail\MailBill;
 use App\Models\Customer;
-use App\Models\Document;
 use App\Models\Rental;
-use Dompdf\Dompdf;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Mail;
@@ -30,22 +28,10 @@ class MailBillJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $dompdf = new Dompdf;
-        $dompdf->loadHtml(view('pdf.bill', ['rental' => $this->rental]));
-        $dompdf->setPaper('A4');
-        $dompdf->render();
-        $filePath = 'docs/bill_'.$this->customer->id.'_'.$this->rental->id.'.pdf';
-        Storage::put($filePath, $dompdf->output());
-
-        Document::create([
-            'type' => DocumentType::BILL,
-            'url' => $filePath,
-            'rental_id' => $this->rental->id,
-        ]);
-
         try {
             print_r("Envoi de l'email de facture à {$this->customer->email} pour la location {$this->rental->id}.\n");
 
+            $filePath = $this->rental->documents()->where('type', DocumentType::BILL->value)->first()->url;
             $success = Mail::to($this->customer->email)->send(new MailBill($this->customer, Storage::path($filePath)));
 
             print_r($this->customer->email.'  : '.($success ? 'Email envoyé' : 'Email non envoyé'));
